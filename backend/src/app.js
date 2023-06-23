5de6e6d684246f52622266914b5afcde7e88870d
jogebro <joelabril2110@gmail.com>
2023-06-23 13:02:58 -0500

    feat: :tada: Doc. Alquilartemis

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+//2. Importar el Framework express
+import express from "express";
+//2.2. Dirigirse a package.json y crear debajo de ("description": "",) a ("type": "module",) 
+
+//3. Guardar a express en una variable
+const app = express();
+
+//4. Exportar a esa constante
+export default app;
---
+++ backend/src/index.js
@@ -0,0 +1,7 @@
+//1. Hacer un metodo main
+const main = () => {
+
+}
+
+//1.2. La invocamos para que se este ejecutando --- (ir a app.js)
+main();
