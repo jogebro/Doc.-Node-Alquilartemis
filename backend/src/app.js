432c2ba13b32e2f89c9f1fc67311830311a44592
jogebro <joelabril2110@gmail.com>
2023-06-23 13:23:45 -0500

    feat:

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,5 +5,8 @@ import express from "express";
 //3. Guardar a express en una variable
 const app = express();
 
-//4. Exportar a esa constante
-export default app;
+//4. Crear el puerto
+app.set("port", 4000);
+
+//5. Exportar a esa constante --- (ir a index.js)
+export default app; 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
-//1. Hacer un metodo main
-const main = () => {
+//6. Importar el app
+import app from "./app.js"
 
+//1. Hacer un metodo main con una function expression
+const main = () => {
+    //7. Utilizamos el app y hacemos que escuche el puerto indicado
+    app.listen(app.get("port")) //app.get selecciona el puerto como lo referenciamos, en este caso "port"
+    console.log(`Alquilartemis is running on port ${app.get("port")}`); //aqui podemos comprovar el consola si se esta ejecutando
+
+    //8. Dirigete a package.json y dentro de ("scripts":) borra el ("text":)
+    //8.1. Creamos ahi mismo a ("dev": "node src/index.js")
+    //8.2. Ejecutar el archivo con   npm run dev
 }
 
 //1.2. La invocamos para que se este ejecutando --- (ir a app.js)
